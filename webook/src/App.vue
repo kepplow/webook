37128825517837b3d1baabf14f2c352075b4b6cf
lilei <1184841584@qq.com>
2019-11-01 17:42:17 +0800

    制作首页自适应框架及部分内容：banner、product部分

--- webook/src/App.vue
+++ webook/src/App.vue
@@ -1,34 +1,84 @@
 <template>
   <main id="app">
     <header>
-      这是头部
+      <b-navbar toggleable="md" type="light" class="site-nav">
+        <b-navbar-brand href="#">岑泽网络科技</b-navbar-brand>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="/" active>首页</b-nav-item>
+          <b-nav-item href="/order">订单</b-nav-item>
+          <b-nav-item href="#">书架</b-nav-item>
+          <b-nav-item href="#">晒单</b-nav-item>
+          <b-nav-item href="#">
+            <span>Rose</span>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-navbar>
+      <b-navbar type="light" class="bottom-nav" fixed="bottom">
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="container">
+          <b-nav-item href="/" active>首页</b-nav-item>
+          <b-nav-item href="/order">订单</b-nav-item>
+          <b-nav-item href="#">书架</b-nav-item>
+          <b-nav-item href="#">晒单</b-nav-item>
+          <b-nav-item href="#">
+            <span>我的</span>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-navbar>
     </header>
-    <router-view/>
-    <footer>
-      这是尾部
-    </footer>
+    <div class="wrapper">
+      <router-view />
+    </div>
+    <footer>这是尾部</footer>
   </main>
 </template>
 
 <style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+html,body {
+  font-size: 14px;
+  background-color: aliceblue;
+  overflow-x: hidden;
+}
+.site-nav {
+  position: relative;
+  z-index: 20;
+}
+.bottom-nav {
+  z-index: 20;
+  background: #fff;
+  transition: 0.3s;
+  border-top: 1px solid rgba(0,0,0,.1);
+  box-shadow: 0 2px 4px rgba(50,50,50,.1);
+}
+.navbar-light .navbar-nav .nav-link.active, .navbar-light .navbar-nav .nav-link:hover{
+  color: #f56368;
+}
+footer {
+  height: 300px;
+  border: 1px solid rgba(0, 0, 0, 0.5);
+}
+@media (max-width: 767.98px) {
+  .site-nav {
+    display: none;
+  }
+  footer {
+    display: none;
+  }
+}
+@media (min-width: 576px) {
+  .bottom-nav {
+    display: none;
   }
 }
 </style>
--- webook/src/views/Home.vue
+++ webook/src/views/Home.vue
@@ -1,13 +1,200 @@
 <template>
-  <div class="home">
-    主页
+  <div class="home container-full">
+    <section class="banner">
+      <div class="swiper-box">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <div
+              data-background="https://picsum.photos/1024/480/?image=55"
+              class="swiper-lazy swiper-img"
+            >
+              <div class="swiper-lazy-preloader"></div>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div
+              data-background="https://picsum.photos/1024/480/?image=58"
+              class="swiper-lazy swiper-img"
+            >
+              <div class="swiper-lazy-preloader"></div>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div
+              data-background="https://picsum.photos/1024/480/?image=52"
+              class="swiper-lazy swiper-img"
+            >
+              <div class="swiper-lazy-preloader"></div>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-pagination"></div>
+      </div>
+    </section>
+    <section class="products">
+      <header>
+        <h1>—我们的产品—</h1>
+        <p>Our Products</p>
+      </header>
+      <b-container class="text-sm-center">
+        <b-row class="justify-content-center">
+          <b-col class="col-sm-6 col-md-4 col-12">
+            <b-card no-body tag="div" class="flex-row flex-sm-column align-items-center">
+              <a href="#" class="w-100">
+                <b-card-img-lazy src="https://picsum.photos/400/400/?image=20" top alt="Image"></b-card-img-lazy>
+              </a>
+              <b-card-body class="py-sm-4">
+                <h4>微信书</h4>
+                <div>微信从此跃然纸上</div>
+                <b-button href="#" variant="link" class="text-danger p-0 mt-2">了解详情></b-button>
+              </b-card-body>
+            </b-card>
+          </b-col>
+          <b-col class="col-sm-6 col-md-4 col-12">
+            <b-card no-body tag="div" class="flex-row flex-sm-column align-items-center">
+              <a href="#" class="w-100">
+                <b-card-img-lazy src="https://picsum.photos/400/400/?image=20" top alt="Image"></b-card-img-lazy>
+              </a>
+              <b-card-body class="py-sm-4">
+                <h4>微信书</h4>
+                <div>微信从此跃然纸上</div>
+                <b-button href="#" variant="link" class="text-danger p-0 mt-2">了解详情></b-button>
+              </b-card-body>
+            </b-card>
+          </b-col>
+          <b-col class="col-sm-6 col-md-4 col-12">
+            <b-card no-body tag="div" class="flex-row flex-sm-column align-items-center">
+              <a href="#" class="w-100">
+                <b-card-img-lazy src="https://picsum.photos/400/400/?image=20" top alt="Image"></b-card-img-lazy>
+              </a>
+              <b-card-body class="py-sm-4">
+                <h4>微信书</h4>
+                <div>微信从此跃然纸上</div>
+                <b-button href="#" variant="link" class="text-danger p-0 mt-2">了解详情></b-button>
+              </b-card-body>
+            </b-card>
+          </b-col>
+          <b-col class="col-sm-6 col-md-4 col-12">1</b-col>
+          <b-col class="col-sm-6 col-md-4 col-12">2</b-col>
+          <b-col class="col-sm-6 col-md-4 col-12">3</b-col>
+          <b-col class="col-sm-6 col-md-4 col-12">2</b-col>
+          <b-col class="col-sm-6 col-md-4 col-12">3</b-col>
+        </b-row>
+      </b-container>
+    </section>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import "swiper/css/swiper.min.css";
+import Swiper from "swiper";
 export default {
-  name: 'home'
-}
+  name: "home",
+  data() {
+    return {
+      slide: 0,
+      sliding: null
+    };
+  },
+  methods: {},
+  mounted() {
+    var mySwiper = new Swiper(".swiper-box", {
+      lazy: {
+        loadPrevNext: true
+      },
+      direction: "horizontal",
+      watchSlidesVisibility: true,
+      observer: true, //修改swiper自己或子元素时，自动初始化swiper
+      observeParents: true, //修改swiper的父元素时，自动初始化swiper
+      //使用分页器
+      pagination: {
+        el: ".swiper-pagination",
+        bulletActiveClass: "bullet-active",
+        clickable: true
+      },
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: true //触碰后自动切换停止
+      }
+    });
+  }
+};
 </script>
+<style lang="less" scoped>
+section {
+  position: relative;
+}
+.row .col {
+  padding: 10px;
+}
+.home {
+  position: relative;
+  .banner {
+    .swiper-slide {
+      position: relative;
+      .swiper-img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-size: cover;
+        border-radius: 5px;
+      }
+    }
+    .swiper-slide:before {
+      content: "";
+      display: block;
+      padding-top: 37.5%;
+    }
+    /deep/.swiper-pagination-bullet {
+      background: #f56368;
+      opacity: 0.2;
+    }
+    /deep/.bullet-active {
+      background: #f56368;
+      opacity: 1;
+    }
+  }
+  .products {
+    padding: 42px 14px 56px;
+    .card {
+      border: 0;
+      box-shadow: 0 12px 40px hsla(0, 0%, 68.6%, 0.2);
+      border-radius: 8px;
+      overflow: hidden;
+      margin-bottom: 20px;
+      .card-img-top {
+        object-fit: cover;
+        transition: 0.3s;
+      }
+      .card-body {
+        h4 {
+          font-size: calc(1.275rem + 0.3vw);
+        }
+        min-width: 11em;
+      }
+    }
+  }
+}
+@media (max-width: 575px) {
+  .card-body {
+    text-align: left;
+  }
+  .row .col {
+    padding: 0;
+  }
+}
+@media (min-width: 767.98px) {
+  .home {
+    margin-top: -56px;
+  }
+}
+@media (max-width: 640px) {
+  .home .banner .swiper-slide:before {
+    content: "";
+    display: block;
+    padding-top: 57.25%;
+  }
+}
+</style>
